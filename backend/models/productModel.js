6b5d1b328284b2abff1f0c765006b14d4081ceb9
Sambit Kumar Dalai <sambitskd3@gmail.com>
2022-01-10 07:58:00 +0530

    product api
    product route,model,controller integrated

---
+++ backend/models/productModel.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter the product price"],
+    maxLength: [8, "Price cann't exceed 8 characters"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  category: {
+    type: String,
+    required: [true, "Please enter product category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [4, "Stock cann't exceed 4 characters"],
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: string,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("product", productSchema);
