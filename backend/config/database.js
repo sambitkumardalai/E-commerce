4c47d3dc1aeac62d0bb5f9bbfd838fb5ae035e56
Sambit Kumar Dalai <sambitskd3@gmail.com>
2022-01-16 16:42:49 +0530

    error handling integrated

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,10 +5,7 @@ const connectDatabase = () => {
     .connect(process.env.DB_URI)
     .then((data) =>
       console.log(`mongodb connected with server:${data.connection.host}`)
-    )
-    .catch((err) => {
-      console.log(err);
-    });
+    );
 };
 
 module.exports = connectDatabase;
