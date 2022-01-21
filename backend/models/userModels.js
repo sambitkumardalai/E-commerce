5af60ba5acf80a78679e36fbf27ca0e2623e9067
Sambit Kumar Dalai <sambitskd3@gmail.com>
2022-01-21 07:40:53 +0530

    user model

---
+++ backend/models/userModels.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+    maxlength: [30, "Name can't exceed 30 characters"],
+    minlength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+    minlength: [8, "Password should be greater than 4 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
