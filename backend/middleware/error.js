f1e00f1715c4dbf33464840f34b2fd74376d51d1
Sambit Kumar Dalai <sambitskd3@gmail.com>
2022-03-23 06:39:52 +0530

    authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,6 +9,22 @@ module.exports = (err, req, res, next) => {
     const message = `Resource not found. Invalid:${err.path}`;
     err = new ErrorHandler(message, 400);
   }
+  // mongoose duplciate key error
+  if (err.code === 11000) {
+    console.log(err);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid,Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Jwt expire erro
+  if (err.name === "TokenExpiredError") {
+    const message = `Json web token is expired,Tryp again`;
+    err = new ErrorHandler(message, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
